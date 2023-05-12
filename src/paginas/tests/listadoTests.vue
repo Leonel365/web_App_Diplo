5b2c23ba50985a422d34c73cfe947b51fe525604
Gabriellllllllllllllll <gsierramarrugo@gmail.com>
2023-05-12 11:33:32 -0500

    Modificar resultadosGenerales y listadoTest
    resultadosGenerales tiene modal
    listadoTest tiene 32 preguntas

--- src/paginas/tests/listadoTests.vue
+++ src/paginas/tests/listadoTests.vue
@@ -28,7 +28,7 @@
                 <a href="/tests/test1">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Escala de Adicción a Videojuegos de Lemmens</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">Es una escala que se utiliza para medir el nivel de adicción a los videojuegos. Este test consta de 20 preguntas que evalúan la frecuencia, intensidad y duración del juego, así como la presencia de síntomas de abstinencia y problemas psicosociales relacionados con el uso excesivo de videojuegos.</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-justify">Es una escala que se utiliza para medir el nivel de adicción a los videojuegos. Este test consta de 32 preguntas que evalúan la frecuencia, intensidad y duración del juego, así como la presencia de síntomas de abstinencia y problemas psicosociales relacionados con el uso excesivo de videojuegos.</p>
                 <div class="grid grid-cols-1">
                     <a href="/tests/test1" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Realizar Test
