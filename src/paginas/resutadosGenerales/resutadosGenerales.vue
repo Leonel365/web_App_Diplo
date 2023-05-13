39e08b15a45e4f9b828d4c3bca1009db6b6f5d85
Gabriellllllllllllllll <gsierramarrugo@gmail.com>
2023-05-12 21:25:53 -0500

    Agregar plantilla grafica resultadosGenerales
    Grafica de barras con chart.js

--- src/paginas/resutadosGenerales/resutadosGenerales.vue
+++ src/paginas/resutadosGenerales/resutadosGenerales.vue
@@ -204,12 +204,34 @@ import test1 from '../tests/test1/test1.vue'
     <button @click="closeModal" class="text-blue-700 hover:underline mt-4">Close</button>
   </div>
 </div>
+
+<Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+
+
 </template>
 
 <script>
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
 export default {
+  name: 'BarChart',
+  components: { Bar },
   data() {
     return {
+      chartData: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{ data: [40, 20, 12] }]
+      },
+      chartOptions: {
+        responsive: true
+      },
       isModalOpen: false
     };
   },
@@ -222,4 +244,5 @@ export default {
     }
   }
 };
+
 </script>
