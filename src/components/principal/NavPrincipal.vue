f2984cfe3be0510ace85a2a57b8af3c9ca6f6312
alex mendoza <alexdylam2016@gmail.com>
2023-06-20 22:20:00 -0500

    cambios por alex

--- src/components/principal/NavPrincipal.vue
+++ src/components/principal/NavPrincipal.vue
@@ -2,7 +2,8 @@
     <nav class="md:bg-gray-800 bg-gray-500 p-1">
         <div class="container mx-auto px-10 w-full">
             <ul class="flex flex-col justify-center md:flex-row mt-1 text-xs md:text-sm">
-                <li>
+                <RouterLink to="/destudiante">
+                    <li> 
                     <a href="#" class="transition hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 
                                           block py-2
                                         text-gray-100 border-b border-gray-400 
@@ -10,7 +11,8 @@
                                           hover:bg-gray-700 
                                           hover:text-gray-200
                                           md:border-0 md:mx-2 md:px-3" aria-current="page">Inicio</a>
-                </li>
+                   </li>
+                </RouterLink>
 
                 <RouterLink to="/tests">
                     <li>
--- src/paginas/Destudiante/estudiante.vue
+++ src/paginas/Destudiante/estudiante.vue
@@ -7,4 +7,36 @@ import NavPrincipal from '../../components/principal/NavPrincipal.vue'
 <template>
 <NavPrincipal />
 
+
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1 class="text-center"><strong>¿Son Adictivos los Videojuegos?</strong></h1>
+
+
+  <p class="justificado">Los videojuegos son potencialmente adictivos. Esto no significa ni que los videojuegos 
+    sean necesariamente adictivos ni que todos los videojugadores sean adictos. 
+
+Sabemos que los videojuegos que se juegan online son más adictivos que aquellos que no.
+ De hecho, la tendencia reciente es la de que los videojuegos se jueguen online, ya sea por equipos 
+ (como el LOL o League of Legends) o de forma individual compitiendo contra otros jugadores (como el Fortnite).
+  Por ejemplo, hoy en día hay muchos juegos en los que se ofrecen comprar (con dinero real) 
+ cajas o sobres sorpresa. Los sobres más caros tiene mayor probabilidad de contener
+  a los mejores futbolistas o las mejores cualidades de un arma. Este mecanismo es idéntico
+   al de los juegos de azar, con un alto potencial adictivo. De esta manera, estos videojuegos 
+   tienen una doble capacidad adictiva: como videojuego online y como juego de azar.</p>
+ 
+   <div class="grid grid-cols-4 gap-5">
+
+<div><img src="@/assets/img/Adiccion.jpg" class="h-40 w-60" /></div>
+<div><img src="@/assets/img/videojuegos.png" class="h-40 w-60" /></div>
+<div><img src="@/assets/img/mente.jpg" class="h-40 w-60" /></div>
+<div><img src="@/assets/img/consume.png" class="h-40 w-60" /></div>
+</div>
+  
+</body>
+</html>
+
+
 </template>
